77b418d6d16f011523f82affdd0bc8604b8b41be
= <hammerrhoads@gmail.com>
2014-07-08 12:40:55 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MatrizDeParesPg::Application.config.secret_key_base = '685842d209a7cd5e11852e925a0cd9e7ef85752050319b3441cea2e33908765b1521614c6e76bca089df95d2f9ce69612de384545e59d6f915ef4f4b87d8c02c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MatrizDeParesPg::Application.config.session_store :cookie_store, key: '_matriz-de-pares-pg_session'
