6d4db7f824fa6ef045b3a070ad1ad97567035cd1
= <hammerrhoads@gmail.com>
2014-08-06 19:09:54 -0300

    RSpec configurado

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Person, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	
+	it "run tests plz" do
+		expect(Person.all.count).to be(10)
+	end
+
 end
