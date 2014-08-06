62daa851d1343b2a6e48b5d4887bf6840e8b32ab
= <hammerrhoads@gmail.com>
2014-08-06 20:48:07 -0300

    Salvando e Deletando dias pareados

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,21 @@
 class Person < ActiveRecord::Base
+
+	def days_paired_with pair
+		field_of_days_paired_with(pair).count
+	end
+
+	def paired_today_with pair
+		PairedDay.create(date: Date.today, person: self, pair: pair)
+	end
+
+	def delete_last_day pair
+		last_day = field_of_days_paired_with(pair).order(date: :desc).first
+		last_day.destroy
+	end
+
+	def field_of_days_paired_with pair
+		PairedDay.where("(person_id = ? AND pair_id = ?) OR (person_id = ? AND pair_id = ?)", self, pair, pair, self)
+	end
+
+	private :field_of_days_paired_with
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,8 +2,52 @@ require 'rails_helper'
 
 RSpec.describe Person, :type => :model do
 	
-	it "run tests plz" do
-		expect(Person.all.count).to be(10)
+	before(:each) do
+		@ariel = Person.where(name: "Ariel").first
+    @patricia = Person.where(name: "Patrícia").first
+    @aline = Person.where(name: "Aline").first
+    @lucas = Person.where(name: "Lucas").first
+	end
+
+	it "insert one day of pairing to Ariel with Patricia" do
+		days_paired = @ariel.days_paired_with(@patricia)
+
+		@ariel.paired_today_with(@patricia)
+
+		ariel_days_bd_paired = @ariel.days_paired_with(@patricia)
+		expect(ariel_days_bd_paired).to be(1)
+	end
+
+	it "get the number of times that one person paired with ariel" do
+		@ariel.paired_today_with(@aline)
+		@ariel.paired_today_with(@aline)
+		@ariel.paired_today_with(@aline)
+		@ariel.paired_today_with(@aline)
+
+		days_paired = @ariel.days_paired_with(@aline)
+
+		expect(days_paired).to be(4)
+	end
+
+	it "reflect pair, when ariel pairing with lucas, both can are changed" do
+		@ariel.paired_today_with(@lucas)
+		@ariel.paired_today_with(@lucas)
+		@ariel.paired_today_with(@lucas)
+
+		days_paired = @lucas.days_paired_with(@ariel)
+
+		expect(days_paired).to be(3)
+	end
+
+	it "delete the last day of paring of one pair" do
+		@ariel.paired_today_with(@lucas)
+		@ariel.paired_today_with(@lucas)
+		@ariel.paired_today_with(@lucas)
+
+		@ariel.delete_last_day(@lucas)
+		days_paired = @ariel.days_paired_with(@lucas)
+
+		expect(days_paired).to be(2)
 	end
 
 end
